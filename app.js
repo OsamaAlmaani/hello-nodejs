6a76a8f3513820a2f8a1c5a20774c8f8337dc4bf
Osama Almaani <osama.maani@gmail.com>
2020-06-11 22:48:10 +0300

    update (1)

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", function (req, res, next) {
-  res.send("hello from nodejs/express");
+app.get('/', function (req, res, next) {
+	res.send('hello from nodejs/express - updated (1)');
 });
 
 app.listen(8080, function () {
-  console.log("CORS-enabled web server listening on port 8080");
+	console.log('CORS-enabled web server listening on port 8080');
 });
